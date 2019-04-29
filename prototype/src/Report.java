830916e4e7f0e66acd6cb9da5c39e1978ad7ef5f
Chris Moorhead <cam0071@auburn.edu>
2019-04-28 22:21:02 -0500

    updated report, abstract sensor

--- prototype/src/Report.java
+++ prototype/src/Report.java
@@ -1,2 +1,21 @@
 public class Report {
+   private String contents;
+   private String reportID;
+   
+   public Report(String reportID) {
+      contents = "";
+      this.reportID = reportID;
+   }
+   
+   public String getContents() {
+      return contents;
+   }
+   
+   public void setContents(String newContents) {
+      contents = newContents;
+   }
+   
+   public String getReportID() {
+      return reportID;
+   }
 }
--- prototype/src/Sensor.java
+++ prototype/src/Sensor.java
@@ -1,2 +1,15 @@
-public class Sensor {
+public abstract class Sensor {
+   protected String serialID;
+   protected boolean status;
+   protected boolean issueDetected;
+   
+   public Sensor(String serialID) {
+      this.serialID = serialID;
+      issueDetected = false;
+      status = true;
+   }
+   
+   public abstract boolean detectIssue();
+
+   public abstract void updateReport(String contents);
 }
